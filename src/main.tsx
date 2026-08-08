e81b459af34a46ce87ce4051fd3741dbf17600b6
griswoldwonders <124472745+griswoldwonders@users.noreply.github.com>
2026-08-07 21:35:53 -0700

    Load mobile dashboard style layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './material'
 import './index.css'
+import './mobile-reference.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
