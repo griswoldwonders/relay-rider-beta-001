51315541e2f80710e2f30f988838bed6423b87a0
K C <124472745+griswoldwonders@users.noreply.github.com>
2026-09-02 06:41:41 -0700

    feat: register operational model modules

--- backend/relay/apps.py
+++ backend/relay/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class RelayConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relay'
@@ -7,6 +8,7 @@ class RelayConfig(AppConfig):
     def ready(self):
         from django.conf import settings
 
+        from . import rule2202_models  # noqa: F401
         from .security_boundary import assert_dev_boundary_safe
 
         assert_dev_boundary_safe(
